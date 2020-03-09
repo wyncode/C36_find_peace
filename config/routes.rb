5efe2b37e6990a57ec70b5f5cd15b57c565f8c17
Trsif <Pepefava1@gmail.com>
2020-03-09 16:28:11 -0400

    Route to staticpage work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
+  get 'staticpage', to: 'staticpage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
