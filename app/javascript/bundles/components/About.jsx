8b4ff70f66500329c83e2cbc9125c550cad671c3
Hannah Shea <hannahshea.consulting@gmail.com>
2020-03-17 13:47:11 -0400

    added css to about page and edited about.jsx, having issues with visibility on local host.

--- app/javascript/bundles/components/About.jsx
+++ app/javascript/bundles/components/About.jsx
@@ -1,28 +1,79 @@
-import React from "react";
-import SheaHannah from "../../../assets/images/SheaHannah.jpg";
-import StRemySonia from "../../../assets/images/StRemySonia.jpg";
-import KeranoraKrisi from "../../../assets/images/KeranoraKrisi.jpg";
-import FavardTristan from "../../../assets/images/FavardTristan.jpg";
+import React from "react"
+import "../../../assets/stylesheets/about.scss"
+import SheaHannah from "../../../assets/images/SheaHannah.jpg"
+import StRemySonia from "../../../assets/images/StRemySonia.jpg"
+import KeranoraKrisi from "../../../assets/images/KeranoraKrisi.jpg"
+import FavardTristan from "../../../assets/images/FavardTristan.jpg"
+import github from "../../../assets/images/github-logo.jpeg"
+import linkedin from "../../../assets/images/linkedin-logo.png"
 
 const About = () => {
   return (
     <>
-      <div className="teamImages">
-        <div id="teamImage">
-          <img src={SheaHannah} alt="TeamImage" id="about-image" />;
+      <div className="flex-picture-names">
+        <div className="profile-block">
+          <p className="names">Hannah Shea</p>
+          <img src={SheaHannah} alt="Hannah" className="about-image" />
+          <div className="inner-flex">
+            <a href="https://github.com/hbshea" target="_blank">
+              <img src={github} alt="github" className="github-logo" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/hannah-shea-aba22b31/"
+              target="_blank"
+            >
+              <img src={linkedin} alt="linkedin" className="linkedin-logo" />
+            </a>
+          </div>
         </div>
-        <div id="teamImage">
-          <img src={StRemySonia} alt="TeamImage" id="about-image" />;
+        <div className="profile-block">
+          <p className="names">Sonia St. Remy</p>
+          <img src={StRemySonia} alt="Sonia" className="about-image" />
+          <div className="inner-flex">
+            <a href="https://github.com/stremysonia" target="_blank">
+              <img src={github} alt="github" className="github-logo" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/sonia-st-remy/"
+              target="_blank"
+            >
+              <img src={linkedin} alt="linkedin" className="linkedin-logo" />
+            </a>
+          </div>
         </div>
-        <div id="teamImage">
-          <img src={KeranoraKrisi} alt="TeamImage" id="about-image" />;
+        <div className="profile-block">
+          <p className="names">Krisi Keranova</p>
+          <img src={KeranoraKrisi} alt="Krisi" className="about-image" />
+          <div className="inner-flex">
+            <a href="https://github.com/krisike" target="_blank">
+              <img src={github} alt="github" className="github-logo" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/krisi-keranova-9bbba819/"
+              target="_blank"
+            >
+              <img src={linkedin} alt="linkedin" className="linkedin-logo" />
+            </a>
+          </div>
         </div>
-        <div id="teamImage">
-          <img src={FavardTristan} alt="TeamImage" id="about-image" />;
+        <div className="profile-block">
+          <p className="names">Tristan Favard</p>
+          <img src={FavardTristan} alt="Tristan" className="about-image" />
+          <div className="inner-flex">
+            <a href="https://github.com/trsif" target="_blank">
+              <img src={github} alt="github" className="github-logo" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/tristan-favard-798453107/"
+              target="_blank"
+            >
+              <img src={linkedin} alt="linkedin" className="linkedin-logo" />
+            </a>
+          </div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default () => <About />;
+export default () => <About />
