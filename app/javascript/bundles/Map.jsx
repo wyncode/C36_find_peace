5a098450fb054e4517dbb9cca110a7e6a4968049
Tommy Suwunrut <Tommy.Suwunrut@gmail.com>
2020-03-16 21:39:37 -0400

    more comments

--- app/javascript/bundles/Map.jsx
+++ app/javascript/bundles/Map.jsx
@@ -18,6 +18,7 @@ const Map = () => {
   const [map, setMap] = useState();
   const [currentUserPositions, setCurrentUserPositions] = useState([]);
 
+  // takes an number[] of coords then calls mapbox's geolocation api to get address for those coords
   const getAddressFromCoordinates = async coords => {
     const { data } = await axios.get(
       `https://api.mapbox.com/geocoding/v5/mapbox.places/${coords[0]},${coords[1]}.json?access_token=${mapboxgl.accessToken}`
@@ -26,26 +27,41 @@ const Map = () => {
     return location ? location.place_name : coords;
   };
 
-  // Adding direction to the top - left hand corner of the map.
   const createMap = (mapOptions, currentUserPositions) => {
+    // initialize the map
     const map = new MapBox(mapOptions);
     setMap(map);
     const { accessToken } = mapboxgl;
+
+    // initializes the three map controls - direction, geolocator, and navigation
     const directionControls = new MapboxDirections({
       accessToken,
       profile: "mapbox/driving"
+      // uncomment out the lines below if you want to disable turn-by-turn insutrctions
+      //   controls: {
+      //       instructions: false
+      //   }
     });
+    const geolocateControls = new GeolocateControl({
+      geolocationOptions,
+      trackUserLocation: true
+    });
+    const navigationControls = new NavigationControl();
+
+    // adds the controls to the map
     map.addControl(directionControls, "top-left");
-    map.addControl(
-      new GeolocateControl({ geolocationOptions, trackUserLocation: true })
-    );
-    map.addControl(new NavigationControl(), "top-right");
+    map.addControl(geolocateControls);
+    map.addControl(navigationControls, "top-right");
+
     map.on("load", async () => {
+      // find the origin and desitnation for the directionControls
       const [origin, destination] = await Promise.all([
         getAddressFromCoordinates(currentUserPositions),
         // please replace these coordinates with shelter coordinates
         getAddressFromCoordinates([-80.1918, 25.7617])
       ]);
+
+      // sets the origin and destination
       directionControls.setOrigin(origin);
       directionControls.setDestination(destination);
     });
@@ -54,36 +70,37 @@ const Map = () => {
   // Adding longitude , latitude and enabled zoom in and zoom out to the map of Miami, Florida.
   // Access token for Mapbox is included in this function.
   const setUpMap = async () => {
+    // gets the current user's location
     const position = await loadPosition();
 
-    // reset these default values to center of miami
+    // change these hardcoded default to whereever you'd like
     const { longitude = -80.1918, latitude = 25.7617 } = position.coords;
+
     const currentUserPositions = [longitude, latitude];
     setCurrentUserPositions(currentUserPositions);
+
     // please hide key when you get the chance
     mapboxgl.accessToken =
       "pk.eyJ1Ijoic3RyZW15c29uaWEiLCJhIjoiY2s3cWprMjRoMDQyYzNmbzFtbGdkMjc4cCJ9.4LDe2fqB-j4eQMROWbd6sA";
+
     const mapOptions = {
       container: "map-container",
       style: "mapbox://styles/mapbox/streets-v11",
       zoom: 14,
       center: currentUserPositions
     };
+
     createMap(mapOptions, currentUserPositions);
   };
 
-  useEffect(() => {
-    setUpMap();
-  }, []);
+  // component did mount
+  useEffect(() => void setUpMap(), []);
 
-  // Giving style (height / width) to the map.
   return (
-    <div>
-      <div
-        style={{ height: 700, width: 800, border: "1px solid black" }}
-        id="map-container"
-      />
-    </div>
+    <div
+      style={{ height: 700, width: 800, border: "1px solid black" }}
+      id="map-container"
+    />
   );
 };
 
