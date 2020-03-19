bcc5f0bf0d851f20b2b09388a5a3470fb3dbdf5c
Sonia <st.remysonia@gmail.com>
2020-03-18 23:03:44 -0400

    used pure css and css to make static page appealing

--- app/javascript/bundles/components/StaticPage.jsx
+++ app/javascript/bundles/components/StaticPage.jsx
@@ -25,19 +25,15 @@ const StaticPage = () => {
       });
   };
   return (
-    <div>
-      <div>
+    <div className= "body">
+      <h1>Find Peace Yoga</h1>
+      <div className= ".1-box" >
         {videos.map(videos => {
           return (
-            <div key={videos.id} className=" pure-g pure-u-1-3 ">
-              {videos.title}
-              <ReactPlayer
-                url={videos.url}
-                light
-                controls
-                width="20%"
-                style={vidStyle}
-              />
+            <div key={videos.id} className=" pure-g pure-u-1-4 .pure-u-lg- pure-u " id="parent">
+                <div className="sub-title"><h5>{videos.title}</h5></div>
+                    <div className= "videos" id= "rows"></div>
+                          <ReactPlayer url={videos.url} light controls width="90%"style={vidStyle}/>
             </div>
           );
         })}
