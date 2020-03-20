35da29aeb8a0bdaf53c599641db695b629768143
Sonia <st.remysonia@gmail.com>
2020-03-20 15:45:50 -0400

    gemoji file added to app

--- app/javascript/bundles/components/StaticPage.jsx
+++ app/javascript/bundles/components/StaticPage.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 import axios from "axios";
+<link rel='stylesheet' 
+media='screen and (min-width: 701px) and (max-width: 900px)' 
+href='css/medium.css' />
+
+
 
 const StaticPage = () => {
   const vidStyle = {
@@ -27,10 +32,9 @@ const StaticPage = () => {
   return (
     <div className= "body">
       <h1>Find Peace Yoga</h1>
-      <div className= ".1-box" >
         {videos.map(videos => {
           return (
-            <div key={videos.id} className=" pure-g pure-u-1-4 .pure-u-lg- pure-u " id="parent">
+            <div key={videos.id} className=" pure-g pure-u-1-3 .pure-u-lg- pure-u " id="parent">
                 <div className="sub-title"><h5>{videos.title}</h5></div>
                     <div className= "videos" id= "rows"></div>
                           <ReactPlayer url={videos.url} light controls width="90%"style={vidStyle}/>
@@ -38,8 +42,99 @@ const StaticPage = () => {
           );
         })}
       </div>
-    </div>
   );
 };
 
 export default () => <StaticPage />;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// import ReactPlayer from "react-player";
+// import axios from "axios";
+
+// const StaticPage = () => {
+//   const vidStyle = {
+  
+//   };
+//   // looping through video data table goes here//
+
+//   const [videos, setVideos] = useState([]);
+
+//   useEffect(() => {
+//     getvideos();
+//   }, []);
+
+//   const getvideos = () => {
+//     axios
+//       .get(`/video`)
+//       .then(res => {
+//         setVideos(res.data);
+//       })
+//       .catch(err => {
+//         console.log(err.res);
+//       });
+//   };
+//   return (
+//     <div className= "body">
+//       <h1>Find Peace Yoga</h1>
+//       <div className= ".1-box" >
+//         {videos.map(videos => {
+//           return (
+//             <div key={videos.id} className=" pure-g pure-u-1-4 .pure-u-lg- pure-u " id="parent">
+//                 <div className="sub-title"><h5>{videos.title}</h5></div>
+//                     <div className= "videos" id= "rows"></div>
+//                           <ReactPlayer url={videos.url} light controls width="90%"style={vidStyle}/>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default () => <StaticPage />;
