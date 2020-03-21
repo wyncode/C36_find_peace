54116de614b198b456d62efc876eae8c866d9f0d
Krisi <kpucu9@gmail.com>
2020-03-20 19:02:07 -0500

    created Modal and change background

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'about', to: 'about#index'
   get 'help', to: 'help#index'
-  # get 'nav', to: 'nav#index'
+  get 'nav', to: 'nav#index'
   get 'staticpage', to: 'staticpage#index'
   get 'yogamap', to: 'yogamap#index'
-  # get 'chatbox', to: 'chatbox#index'
   resources :organizations, only: [:index]
   resource :chat, only: [:show]
   get 'video', to: 'video#index'
