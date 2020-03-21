820262e06c3b61ae3d4325091aeb5d53d445ce82
Sonia <st.remysonia@gmail.com>
2020-03-21 15:49:06 -0400

    made the video page modile friendly and the star from chat css

--- app/javascript/bundles/components/StaticPage.jsx
+++ app/javascript/bundles/components/StaticPage.jsx
@@ -3,7 +3,9 @@ import ReactPlayer from "react-player";
 import axios from "axios";
 
 const StaticPage = () => {
-  const vidStyle = {
+  const vidStyle = {display:"block",
+    marginLeft: "auto",
+    marginRight: "auto"
   
   };
   // looping through video data table goes here//
@@ -25,15 +27,15 @@ const StaticPage = () => {
       });
   };
   return (
-    <div className= "body">
+    <div>
       <h1>Find Peace Yoga</h1>
-      <div className= ".1-box" >
+      <div >
         {videos.map(videos => {
           return (
-            <div key={videos.id} className=" pure-g pure-u-1-4 .pure-u-lg- pure-u " id="parent">
+            <div className= "container" key={videos.id}>
                 <div className="sub-title"><h5>{videos.title}</h5></div>
-                    <div className= "videos" id= "rows"></div>
-                          <ReactPlayer url={videos.url} light controls width="90%"style={vidStyle}/>
+                    <div className= "videos"></div>
+                          <ReactPlayer url={videos.url} light controls style={vidStyle}/>
             </div>
           );
         })}
