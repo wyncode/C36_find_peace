7e77d55f672d0ccf040598966a75f4420c3d27ae
Sonia <st.remysonia@gmail.com>
2020-03-22 13:42:28 -0400

    resolving conflicting code for the staticpage

--- app/javascript/bundles/components/StaticPage.jsx
+++ app/javascript/bundles/components/StaticPage.jsx
@@ -1,11 +1,7 @@
+ 
 import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 import axios from "axios";
-<link rel='stylesheet' 
-media='screen and (min-width: 701px) and (max-width: 900px)' 
-href='css/medium.css' />
-
-
 
 const StaticPage = () => {
   const vidStyle = {display:"block",
@@ -13,7 +9,7 @@ const StaticPage = () => {
     marginRight: "auto"
   
   };
-  // looping through video data table goes here
+  // looping through video data table goes here//
 
   const [videos, setVideos] = useState([]);
 
@@ -34,10 +30,10 @@ const StaticPage = () => {
   return (
     <div>
       <h1>Find Peace Yoga</h1>
+      <div >
         {videos.map(videos => {
           return (
-            <div key={videos.id} className=" pure-g pure-u-1-3 .pure-u-lg- pure-u " id="parent">
-
+            <div className= "container" key={videos.id}>
                 <div className="sub-title"><h5>{videos.title}</h5></div>
                     <div className= "videos"></div>
                           <ReactPlayer url={videos.url} light controls style={vidStyle}/>
@@ -45,6 +41,7 @@ const StaticPage = () => {
           );
         })}
       </div>
+    </div>
   );
 };
 
