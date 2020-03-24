0cd928e59a030a6cf1ed67cfde8a41d7c77b7536
Trsif <Pepefava1@gmail.com>
2020-03-23 21:47:42 -0400

    Heroku-push

--- app/javascript/bundles/components/StaticPage.jsx
+++ app/javascript/bundles/components/StaticPage.jsx
@@ -1,23 +1,18 @@
- 
 import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 import axios from "axios";
-import about from "../../../assets/images/find-peace-yoga.png"
-
+import about from "../../../assets/images/find-peace-yoga.png";
 const StaticPage = () => {
-  const vidStyle = {display:"block",
+  const vidStyle = {
+    display: "block",
     marginLeft: "auto",
     marginRight: "auto"
-  
   };
   // looping through video data table goes here//
-
   const [videos, setVideos] = useState([]);
-
   useEffect(() => {
     getvideos();
   }, []);
-
   const getvideos = () => {
     axios
       .get(`/video`)
@@ -29,26 +24,25 @@ const StaticPage = () => {
       });
   };
   return (
-  <div id="master-div">
-    <div className="static-page-banner"> <img src={about} alt="static-page" className="static-page-banner" /></div>
+    <div id="master-div">
+      <div className="static-page-banner">
+        {" "}
+        <img src={about} alt="static-page" className="static-page-banner" />
+      </div>
       <div>
         {videos.map(videos => {
           return (
-            <div className= "container" key={videos.id}>
-                <div className="sub-title"><h5>{videos.title}</h5></div>
-                    <div className= "videos"></div>
-                          <ReactPlayer url={videos.url} light controls style={vidStyle}/>
+            <div id="container" key={videos.id}>
+              <div className="sub-title">
+                <h5>{videos.title}</h5>
+              </div>
+              <div className="videos"></div>
+              <ReactPlayer url={videos.url} light controls style={vidStyle} />
             </div>
           );
         })}
       </div>
     </div>
-  
   );
 };
-
 export default () => <StaticPage />;
-
-
-
-
